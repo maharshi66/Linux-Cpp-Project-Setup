714793d4d8902ce4134bf3719aea28b96a5f7e1d
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-02-05 19:58:18 +0530

    Added main makefile for the project with project structure setup

--- cpp_project_name/analysis/cpp check/beforeScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-apt update
-apt -y install cppcheck
-apt -y install python-pygments
---
+++ cpp_project_name/analysis/cppCheck/beforeScript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y cppcheck
+apt-get -y install python-pygments
